9995121aca45143c8e2d1a74bf12068ab6bb0e99
HEOSEONJU <zxaz741@gmail.com>
2023-08-08 15:53:08 +0900

    feat:카메라 좌우이동추가

--- SandCastle/Assets/CreateSJ/InGame/InGame_Camera_Move.cs
+++ SandCastle/Assets/CreateSJ/InGame/InGame_Camera_Move.cs
@@ -10,42 +10,17 @@ namespace InGame
         float clampPlusX;
         [SerializeField]
         float clampMinusX;
-        [SerializeField]
-        float clampPlusY;
-        [SerializeField]
-        float clampMinusY;
-        
-        
-        [SerializeField]
-        Vector3 origin = new Vector3(-0.31f, 0, -10f);
 
 
-        public void Clamp_Camera(Transform master)
-        {
-            
-            transform.position = Vector3.Lerp(transform.position, master.position + origin,
-                                  Time.deltaTime * 1f);
 
-            
-            Vector3 Temp = transform.position;
 
-            if(Temp.x<= (clampMinusX) ||Temp.x>= clampPlusX || Temp.y<=(clampMinusY) ||Temp.y>= clampPlusY)
-            {
-                Temp.x = Mathf.Clamp(Temp.x, clampMinusX, clampPlusX);
-                Temp.y = Mathf.Clamp(Temp.y, clampMinusY, clampPlusY);
-                
-                transform.position = Temp;
-                return;
-            }
-            
-
-        }
-        public void TraceChar(Transform posi)
+        public void TraceChar(float x,float y)
         {
             Vector3 temp = transform.position;
+            temp.x = x;
             temp.x = Mathf.Clamp(temp.x, clampMinusX, clampPlusX);
-            temp.y=posi.transform.position.y;
-
+            temp.y = y;
+            
 
             transform.position = temp;
         }
